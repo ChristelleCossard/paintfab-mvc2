f0d092575ab00324b470b8997b6e21188aeddc66
Christelle Cossard <christelle.cossard10@gmail.com>
2023-04-07 12:41:20 +0200

    travail3 07 avril 2023

---
+++ administration.php
@@ -0,0 +1,32 @@
+<?php
+require_once('templates/header.php');
+require_once('lib/user.php');
+
+if(!isset($_SESSION['user']) && $user['role'] == "client") {
+    header('location: login.php');
+}
+
+require_once('lib/tools.php');
+//require_once('lib/dish.php');
+//require_once('lib/category.php');
+
+?>
+
+<h1 align="center">Page d'administration</h1>
+<br>
+<h2>Voici la liste des opérations que vous pouvez effectuer:</h2>
+<br>
+<p class="nav-item"><a href="ajout_modification_plat.php" class="btn btn-primary <?php if ($currentPage === $key) { echo 'active'; } ?>">Ajouter un plat</a>
+<br><br><br>
+<?php
+
+var_dump($_SESSION);
+
+
+
+
+?>
+
+<?php
+require_once('templates/footer.php');
+?>
