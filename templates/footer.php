b32f0e1a6c5fea5b8204f45ec943a624f8f2b5ea
Christelle Cossard <christelle.cossard10@gmail.com>
2023-04-07 11:14:19 +0200

    ajout des fichiers du dossier lib

--- templates/footer.php
+++ templates/footer.php
@@ -4,11 +4,9 @@
 
 <footer class="py-3 my-4">
     <ul class="nav justify-content-center border-bottom pb-3 mb-3">
-        <li class="nav-item"><a href="#" class="nav-link px-2 text-muted">Home</a></li>
+        <li class="nav-item"><a href="/" class="nav-link px-2 text-muted">Accueil</a></li>
         <li class="nav-item"><a class="nav-link" href="legal.php">Mentions légales</a></li>
-        <li class="nav-item"><a href="#" class="nav-link px-2 text-muted">Pricing</a></li>
-        <li class="nav-item"><a href="#" class="nav-link px-2 text-muted">FAQs</a></li>
-        <li class="nav-item"><a href="#" class="nav-link px-2 text-muted">About</a></li>
+
     </ul>
     <p class="text-center text-muted">© 2023 <a class="nav-link" href="http://cocoweb10.fr/" target="_blank"> Christelle Cossard.</a></p>
 </footer>
