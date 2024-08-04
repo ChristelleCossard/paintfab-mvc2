3ae91e5dcad0998673b98813c9254ba93df7d1f5
Christelle Cossard <christelle.cossard10@gmail.com>
2024-08-04 10:00:08 +0200

    essais connexion

--- lib/config.php
+++ lib/config.php
@@ -2,6 +2,22 @@
   define('_PRODUCTS_IMG_PATH_', 'uploads/products/');
   define('_ASSETS_IMAGES_FOLDER_', '/images/');
   define('_HOME_PRODUCTS_LIMIT_', 6);
+/*
+  define("_DB_SERVER_", "mysql-cocodev.alwaysdata.net");
+  define("_DB_NAME_", "cocodev_garage");
+  define("_DB_USER_", "cocodev");
+  define("_DB_PASSWORD_", "Vicky.10");
+*/
+/*
+  define("_DB_SERVER_", "127.0.0.1");
+  define("_DB_NAME_", "paintfab");
+  define("_DB_USER_", "root");
+  define("_DB_PASSWORD_", "");
+*/
+
+
+//$pdo = new PDO('mysql:dbname=paintfab;host=127.0.0.1;port=3308;charset=utf8mb4', 'root', '');
+
 
   $mainMenu = [
     'index.php' => 'Accueil', 
--- lib/pdo.php
+++ lib/pdo.php
@@ -1,3 +1,15 @@
 <?php
 //$pdo = new PDO('mysql:dbname=cocoresto;host=127.0.0.1;port=3308;charset=utf8mb4', 'root', '');
+//$pdo = new PDO('mysql:dbname=cocodev_paintfab;host=mysql-cocodev.alwaysdata.net;cocodev, Vicky.10');
 $pdo = new PDO('mysql:dbname=paintfab;host=127.0.0.1;port=3308;charset=utf8mb4', 'root', '');
+
+/*
+try
+{
+    $pdo = new PDO("mysql:dbname="._DB_NAME_.";host="._DB_SERVER_.";charset=utf8mb4", _DB_USER_, _DB_PASSWORD_);
+}
+catch (Exception $e)
+{
+        die('Erreur : ' . $e->getMessage());
+}
+*/
